d10fa3756c8bdf98b90e7f6e545ab5653c4a2f21
nopeholmes <nopeholmes@gmail.com>
2024-06-23 21:41:53 +0700

    init

---
+++ src/lib/components/HomeHeader.svelte
@@ -0,0 +1,20 @@
+<section class="px-4 py-6">
+  <div class="h-40 w-40 overflow-hidden rounded-full">
+    <img
+      src="/profile-pic-for-blog.jpg"
+      alt="Profile"
+      class="object-cover"
+    />
+  </div>
+
+  <div class="space-y-2 py-4">
+    <h2 class="text-4xl font-bold">Hai! Nama Saya Sunu.</h2>
+    <p class="text-2xl font-light">
+      I want to sharing what I know.
+    </p>
+    <p class="text-2xl font-thin">
+      You can find me on my youtube channel and I also like to blog
+      here to supplement the videos I make!
+    </p>
+  </div>
+</section>
